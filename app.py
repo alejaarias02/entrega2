9e14da30c774b133038faf96690ce2e1ab8c300a
alejaarias02 <147355941+alejaarias02@users.noreply.github.com>
2023-10-08 21:41:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,23 @@
+import streamlit as st
 from googletrans import Translator
 from gtts import gTTS
-import os
 
-# Solicita la frase y el idioma de destino
-frase = input("Ingresa la frase que quieres traducir: ")
-idioma_destino = input("Ingresa el idioma de destino (por ejemplo, 'es' para español): ")
-
-# Crea una instancia del traductor y traduce la frase al idioma de destino
-traductor = Translator()
-traduccion = traductor.translate(frase, dest=idioma_destino)
-frase_traducida = traduccion.text
-
-# Crea una instancia de gTTS para generar el audio
-def text_to_speech(text, tld):
-
-    tts = gTTS(text, lang=tld, slow=False)
-
-    try:
-
-        my_file_name = text[0:20]
-
-    except:
-
-        my_file_name = "audio"
-
-    tts.save(f"temp/{my_file_name}.mp3")
-
-    return my_file_name, textt
-
-
-# Reproduce el archivo de audio usando playsound
-#result, output_text = text_to_speech(translated_text, target_lang_code)
-
-
- 
-    #audio_file = open(f"temp/{result}.mp3", "rb")
-
-    #audio_bytes = audio_file.read()
+st.title("Aplicativo AMA")
+st.image("translate.png", width=200)
+st.subheader("Texto a audio y traducción.")
+text = st.text_input("Ingrese el texto:")
+languages = {"Inglés": "en", "Español": "es", "Chino Mandarín": "zh-cn", "Francés": "fr", "Ruso": "ru", "Hindi": "hi"}
+target_lang = st.selectbox("Seleccione el idioma de destino:", list(languages.keys()))
+if text and target_lang:
+    source_lang = "es"
+    target_lang_code = languages[target_lang]
+    translator = Translator()
+    translated_text = translator.translate(text, src=source_lang, dest=target_lang_code).text
+    tts = gTTS(translated_text, lang=target_lang_code, slow=False)
+    tts.save("temp/audio.mp3")
+    audio_file = open("temp/audio.mp3", "rb")
+    audio_bytes = audio_file.read()
+    st.markdown("## Tu audio:")
+    st.audio(audio_bytes, format="audio/mp3", start_time=0)
+    st.markdown("## Texto en audio:")
+    st.write(translated_text)

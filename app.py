fa5aad228dc2bb776c080a462e91c5298faede57
alejaarias02 <147355941+alejaarias02@users.noreply.github.com>
2023-10-08 22:01:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from googletrans import Translator
 from gtts import gTTS
 
-st.title("Aplicativo AMA")
-#st.image("translate.png", width=200)
+st.title("Aplicativo")
+st.image("translate.png", width=200)
 st.subheader("Texto a audio y traducción.")
 text = st.text_input("Ingrese el texto:")
 languages = {"Inglés": "en", "Español": "es", "Chino Mandarín": "zh-cn", "Francés": "fr", "Ruso": "ru", "Hindi": "hi"}
